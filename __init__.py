cb980ce4d04203662d93abe1dea01f18e68f304c
eddiedigits <eddiedigits@gmail.com>
2024-06-16 03:43:25 +0200

    relative import error handling

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 
-from .nfon_api_base_client import NfonApiBaseClient
+try:
+    from .nfon_api_base_client import NfonApiBaseClient
+except ImportError:
+    from modules.nfon_api.nfon_api_base_client import NfonApiBaseClient
